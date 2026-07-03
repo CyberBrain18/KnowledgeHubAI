4d06125195d573ab81a520c42ad1a3d2af1339fb
CyberBrain18 <srihari.nambiar05@gmail.com>
2026-07-03 16:12:06 +0530

    fix: trying connect local llm model through cloudflare tunnel

--- server/src/llm/llm.service.ts
+++ server/src/llm/llm.service.ts
@@ -1,13 +1,10 @@
 import { Ollama } from "ollama";
-
-console.log("OLLAMA_HOST at startup:", process.env.OLLAMA_HOST);
-
-const ollama = new Ollama({
-  host: process.env.OLLAMA_HOST,
-});
-
 export async function generateAnswer(prompt: string) {
-  console.log("Using OLLAMA_HOST:", process.env.OLLAMA_HOST);
+  console.log("OLLAMA_HOST =", process.env.OLLAMA_HOST);
+
+  const ollama = new Ollama({
+    host: process.env.OLLAMA_HOST!,
+  });
 
   const response = await ollama.chat({
     model: process.env.OLLAMA_CHAT_MODEL!,
